ac067dcb59934b0d7b325d3ebb7e9583607923f5
André Milani <andremilani34@gmail.com>
2026-02-15 12:42:57 -0300

    fix: ajustando responsividade do header, aboutMe e myProjects

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,18 +6,29 @@ export function Header() {
   const [hambMenuOpen, setHambMenuOpen] = useState(false);
   const menuRef = useRef(null);
 
-  // Fecha ao clicar fora
+  // Fecha ao clicar fora (Quando o menu abrir ou fechar, execute isso.)
   useEffect(() => {
+    // Esse bloco é a função que roda sempre que o usuário clica na tela.
     function handleClickOutside(event) {
+      // MenuRef.current → é o menu:
+      //    .contains(event.target) → verifica se o clique foi dentro
+      //    '!' → significa “não”
+      //    Se o clique NÃO foi dentro do menu → fecha.
       if (menuRef.current && !menuRef.current.contains(event.target)) {
+        // Se o clique NÃO foi dentro do menu → fecha.
         setMenuOpen(false);
       }
     }
 
+    // Só escute os cliques se o menu estiver aberto.
     if (hambMenuOpen) {
       document.addEventListener("mousedown", handleClickOutside);
     }
 
+    // Esse bloco remove o evento quando:
+    //    O menu fecha
+    //    O componente desmonta
+    //    Isso evita bugs e eventos duplicados.
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
@@ -36,7 +47,7 @@ export function Header() {
           className={`hamburger ${hambMenuOpen ? "open" : ""}`}
           onClick={() => setHambMenuOpen(!hambMenuOpen)}
         >
-          ☰
+          <i class="fa-solid fa-bars"></i>
         </button>
 
         <nav ref={menuRef} className={`nav ${hambMenuOpen ? "active" : ""}`}>
